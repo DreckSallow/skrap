1cfacd68165727915a27b5d195b6ff128e9c16f4
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-10-27 22:54:28 -0500

    add query all count method

--- src/deps/extractor/src/lib.rs
+++ src/deps/extractor/src/lib.rs
@@ -1,11 +1,6 @@
 use scraper::{Html, Selector};
 use wasm_bindgen::prelude::*;
 
-#[was_bindgen]
-pub fn greet(name: &str) -> String {
-    format!("Hola {}", name)
-}
-
 #[wasm_bindgen]
 pub struct Extractor {
     document: Html,
@@ -24,5 +19,10 @@ impl Extractor {
         let one_sel = selects.next()?;
         let s = one_sel.text().collect::<String>();
         Some(s)
+    }
+    pub fn query_all_count(&self, selector_str: &str) -> Option<u32> {
+        let selector = Selector::parse(selector_str).ok()?;
+        let mut selects = self.document.select(&selector);
+        Some(selects.count() as u32)
     }
 }
