c035df091890fcb8c01f68329ccafee694f8bb8f
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-10-28 23:04:34 -0500

    add query classes

---
+++ src/utils/query.ts
@@ -0,0 +1,55 @@
+import { FormControl } from "@angular/forms";
+
+export type QueryGroupType = "group" | "list";
+
+export class QueryGroup {
+  name: string;
+  open: boolean;
+  type: QueryGroupType;
+  count: number;
+  private _parentSelector: FormControl<string> | null;
+  private _queries: Array<QueryInfo>;
+  constructor(name: string, open: boolean, type: QueryGroupType) {
+    this.name = name;
+    this.open = open;
+    this.type = type;
+    this.count = 0;
+    this._queries = [];
+    if (this.type == "list") this._parentSelector = new FormControl("") as FormControl<string>;
+    else this._parentSelector = null;
+  }
+  get parentSelector() {
+    return this._parentSelector?.value ?? null
+  }
+  set parentSelector(val: string | null) {
+    this._parentSelector = val ? new FormControl(val) as FormControl<string> : null;
+  }
+  get queries() {
+    return this._queries
+  }
+
+  getQuery(i: number) {
+    return this._queries.at(i) ?? null;
+  }
+  addQuery(query: QueryInfo) {
+    this._queries.push(query);
+  }
+  removeQuery(i: number) {
+    return this._queries.splice(i, 1).at(0) ?? null;
+  }
+  removeAllQueries() {
+    this._queries = [];
+  }
+}
+
+
+export class QueryInfo {
+  name: FormControl<string>;
+  selector: FormControl<string>;
+  result: string | null
+  constructor(name: string) {
+    this.name = new FormControl(name) as FormControl<string>;
+    this.selector = new FormControl("") as FormControl<string>;
+    this.result = null;
+  }
+}
