f4f9de6d7774ea0e7b194990140b773716b95e8c
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-11-05 17:36:32 -0500

    add good info for fetch status and handle other errors

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Extractor } from 'src/deps/extractor/pkg';
-const extractor = import("src/deps/extractor/pkg");
+//TODO: Find another way to use wasm files.
+//While not remove the below line, this is used to load wasm firstly. :v
+import("src/deps/extractor/pkg");
+
+interface Info {
+  status: "loading" | "error" | "success";
+  message: string;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -9,25 +17,50 @@ const extractor = import("src/deps/extractor/pkg");
   styleUrls: ['./dashboard.component.css']
 })
 export default class DashboardComponent implements OnInit {
-  url: string | null = null;
+  info: Info = {
+    message: "",
+    status: "loading"
+  }
+  urlForm = new FormGroup({
+    url: new FormControl("", Validators.required)
+  });
   extractor: null | Extractor = null;
 
   constructor(
     private route: ActivatedRoute,
-  ) { }
+    private router: Router
+  ) {
+  }
 
   ngOnInit(): void {
-    extractor.then(({ Extractor }) => {
-      fetch(`http://localhost:8000/skrap?url=${this.url}`).then(async (r) => {
-        this.extractor = Extractor.new(await r.text());
-      }).catch((e) => {
-        console.log("ERROR FETCHING: ", e);
-      })
-    }).catch(() => {
-      console.log("Error loading wasm.");
-    });
     this.route.queryParamMap.subscribe((params) => {
-      this.url = params.get("url");
+      const url = params.get("url");
+      this.urlForm.setValue({ url });
+      if (!url) {
+        return this.setInfo("error", "Enter a valid url before to continue.")
+      }
+      this.createExtractor(url);
     })
+  }
+  private setInfo(status: Info["status"], message: Info["message"]) {
+    this.info.message = message;
+    this.info.status = status;
+  }
+
+  createExtractor(url: string) {
+    this.setInfo("loading", "Loading data...");
+    fetch(`http://localhost:8000/skrap?url=${url}`).then(async (r) => {
+      this.extractor = Extractor.new(await r.text());
+      this.setInfo("success", "");
+    }).catch((_e) => {
+      this.setInfo("error", "An error ocurred. Try reload or enter a valid url :).")
+    })
+  }
+
+  changeUrl() {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { url: this.urlForm.value.url },
+    });
   }
 }
